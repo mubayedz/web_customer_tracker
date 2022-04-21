bfa02185e2fb9b5496317d6f7ba44dc3819001d8
mubayedz <Juan@DESKTOP-RLRH6O3.home>
2022-04-21 23:04:53 +0200

    updated files 1.2

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -18,17 +18,44 @@ public class CustomerDAOImpl implements CustomerDAO {
 	private SessionFactory sessionFactory;
 	
 	@Override
-	@Transactional
 	public List<Customer> getCustomers() {
 		
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
-		// create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		// create a query .. sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
 		// return the results
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save or update the customer
+		currentSession.saveOrUpdate(customer);
+	}
+
+	@Override
+	public Customer getCustomers(int theId) {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// now retrieve/read from database using the primary key
+		Customer customer = currentSession.get(Customer.class, theId);
+		return customer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// delete the customer
+		Query query = currentSession.createQuery("delete from Customer where id=:customerId");
+	    query.setParameter("customerId", theId);
+	    query.executeUpdate();
 	}
 
 }
